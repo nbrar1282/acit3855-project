4754ad8afdf665d07395e1f6659288475043e467
Navdeep Singh Brar <123911801+nbrar1282@users.noreply.github.com>
2025-03-29 19:40:11 -0700

    Update reset_kafka.sh

--- reset_kafka.sh
+++ reset_kafka.sh
@@ -4,4 +4,4 @@ echo "🛠 Removing old Kafka metadata..."
 rm -rf /kafka/meta.properties  # Deletes metadata inside Kafka container
 
 echo "✅ Starting Kafka..."
-exec /usr/bin/start-kafka.sh  # Start Kafka normallygg
+exec /usr/bin/start-kafka.sh  # Start Kafka normallyggg
